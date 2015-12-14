dbf9b00d9112aa8318a4a56a07114f8812797b2d
John Le <john.le@bonniercorp.com>
2015-12-14 11:53:27 -0500

    Add memcached to sitelist

--- aliases.drushrc.php
+++ aliases.drushrc.php
@@ -1,6 +1,20 @@
 <?php
+
+$settings['memcache'] = array('unix:///var/run/memcached/memcached_sitelist.sock');
+
 require_once('aliases.class.php');
 
-$aliases = new LocalBuildDrushAliases();
+$memcache = new Memcache();
+$memcache->addServer($settings['memcache'][0], 0);
+$drush_aliases = $memcache->get('drush_aliases');
 
-$aliases = $GLOBALS['bonnier_drush_aliases'] = $aliases->getAliases();
+if (!empty($drush_aliases)) {
+  $aliases = $drush_aliases;
+}
+else{
+  $aliases = new LocalBuildDrushAliases();
+  
+  $aliases = $GLOBALS['bonnier_drush_aliases'] = $aliases->getAliases();
+  $memcache->set('drush_aliases', $aliases, 0, 3600 * 24);
+}
+
