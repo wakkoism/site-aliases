083a6ec65378b6f502d085bfb5b00bb178bf04cd
John Le <john.le@bonniercorp.com>
2016-10-26 13:52:03 -0400

    Dont build the aliases list if caching exist for aliases.

--- aliases.drushrc.php
+++ aliases.drushrc.php
@@ -1,15 +1,13 @@
 <?php
 require_once('aliases.settings.php');
-require_once('aliases.class.php');
-
 $memcache = new Memcache();
 $memcache->addServer(MEMCACHE_DRUSH_ALIASES, MEMCACHE_PORT_DRUSH_ALIASES);
 $drush_aliases = $memcache->get('drush_aliases');
-
 if (!empty($drush_aliases)) {
   $aliases = $drush_aliases;
 }
 else{
+  require_once('aliases.class.php');
   $aliases = new LocalBuildDrushAliases(SITELIST_URL);
   $aliases = $GLOBALS['bonnier_drush_aliases'] = $aliases->getAliases();
   foreach ($aliases as $name => &$alias) {
